e7654b045bf365d3323fcb0ed92a9beddcfe86f3
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-03-16 12:01:34 -0500

    Plot price over quarters (four times per year).

--- python/make_plots.py
+++ python/make_plots.py
@@ -4,14 +4,29 @@
 # Use the library cryptocompare. 
 # https://pypi.org/project/cryptocompare/
 
+import numpy as np
 import cryptocompare
 import datetime
 import plot
 import tools
 
+# Get price over quarters
+def getPriceOverQuarters(coin, currency, start_year, end_year):
+    print(f" - Collecting price data over quarters: {coin} in {currency} over the year range [{start_year}, {end_year}]")
+    prices = []
+    for year in range(start_year, end_year + 1):
+        for month in range(1, 13, 3):
+            # for the end year, only include the first month
+            if year == end_year and month > 1:
+                break
+            price_object = cryptocompare.get_historical_price(coin, currency, datetime.datetime(year,month,1))
+            price = price_object[coin][currency]
+            prices.append(price)
+    return np.array(prices)
+
 # Get price over years
 def getPriceOverYears(coin, currency, start_year, end_year):
-    print(f" - Collecting data: {coin} in {currency} over the year range [{start_year}, {end_year}]")
+    print(f" - Collecting price data over years: {coin} in {currency} over the year range [{start_year}, {end_year}]")
     prices = []
     for year in range(start_year, end_year + 1):
         price_object = cryptocompare.get_historical_price(coin, currency, datetime.datetime(year,1,1))
@@ -27,14 +42,28 @@ def makePlots(plot_dir):
     
     for coin in coins:
         currency    = 'USD'
+        
+        # plot price vs years
         output_name = f"{plot_dir}/price_{coin}_in_{currency}_vs_years"
         title       = f"Price of {coin} in {currency}"
         x_label     = "Year"
         y_label     = f"Price ({currency})"
         
-        years   = list(range(start_year, end_year + 1))
-        prices  = getPriceOverYears(coin, currency, start_year, end_year)
-        plot.plot_scatter(output_name, years, prices, title, x_label, y_label)
+        # include the end year
+        x_values    = np.arange(start_year, end_year + 1)
+        y_values    = getPriceOverYears(coin, currency, start_year, end_year)
+        plot.plot_scatter(output_name, x_values, y_values, title, x_label, y_label)
+        
+        # plot price vs quarters
+        output_name = f"{plot_dir}/price_{coin}_in_{currency}_vs_quarters"
+        title       = f"Price of {coin} in {currency}"
+        x_label     = "Year (Quarters)"
+        y_label     = f"Price ({currency})"
+        
+        # only inclue the first quarter of the end year
+        x_values    = np.arange(start_year, end_year + 0.25, 0.25)
+        y_values    = getPriceOverQuarters(coin, currency, start_year, end_year)
+        plot.plot_scatter(output_name, x_values, y_values, title, x_label, y_label)
 
 def main():
     print("It's go time.")
