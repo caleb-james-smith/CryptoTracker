0296e2e982825d2513e35a32cf049e2b15fc9620
Caleb James Smith <colorado.caleb.smith@gmail.com>
2024-03-16 11:10:22 -0500

    Save plots.

--- python/make_plots.py
+++ python/make_plots.py
@@ -6,6 +6,7 @@ import plot
 import tools
 
 def getPriceOverYears(coin, currency, start_year, end_year):
+    print(f" - Collecting data: {coin} in {currency} over the year range [{start_year}, {end_year}]")
     prices = []
     for year in range(start_year, end_year + 1):
         price_object = cryptocompare.get_historical_price(coin, currency, datetime.datetime(year,1,1))
@@ -18,13 +19,15 @@ def makePlots(plot_dir):
     end_year    = 2024
     coin        = 'BTC'
     currency    = 'USD'
+    
+    output_name = f"{plot_dir}/price_{coin}_in_{currency}_vs_years"
     title       = f"Price of {coin} in {currency}"
     x_label     = "Year"
     y_label     = f"Price ({currency})"
     
-    years = list(range(start_year, end_year + 1))
-    prices = getPriceOverYears(coin, currency, start_year, end_year)
-    plot.plot_scatter(years, prices, title, x_label, y_label)
+    years   = list(range(start_year, end_year + 1))
+    prices  = getPriceOverYears(coin, currency, start_year, end_year)
+    plot.plot_scatter(output_name, years, prices, title, x_label, y_label)
 
 def main():
     print("It's go time.")
@@ -37,7 +40,11 @@ def main():
     plot_dir = "plots"
     tools.makeDir(plot_dir)
     makePlots(plot_dir)
+    
+    print("Done.")
 
 if __name__ == "__main__":
     main()
+
+
 
--- python/plot.py
+++ python/plot.py
@@ -2,11 +2,20 @@
 
 import matplotlib.pyplot as plt
 
-def plot_scatter(x_values, y_values, title, x_label, y_label):
+def plot_scatter(output_name, x_values, y_values, title, x_label, y_label):
+    # create plot
+    print(f" - Creating plot: {output_name}")
     plt.scatter(x_values, y_values)
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    plt.show()
+    
+    # save plot
+    #plt.show()
+    output_png = f"{output_name}.png"
+    output_pdf = f"{output_name}.pdf"
+    plt.savefig(output_png, bbox_inches='tight')
+    plt.savefig(output_pdf, bbox_inches='tight')
+    plt.close('all')
 
 
